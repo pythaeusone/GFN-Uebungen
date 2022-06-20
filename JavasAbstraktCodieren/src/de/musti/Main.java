6256145f5666867a40d7218c74bd005e9d0cb164
Pythaeus by MuFa <mustafa.jukic@gfn.education>
2022-06-20 21:26:34 +0200

    AlgoTest

--- JavasAbstraktCodieren/src/de/musti/Main.java
+++ JavasAbstraktCodieren/src/de/musti/Main.java
@@ -5,9 +5,9 @@ public class Main {
 	public static void main(String[] args) {
 		// :-)
 		SDV eins = new Vigenere();
-		eins.setPrivateKey("PRUEFUNG");
-		eins.setKlarText("DERADLERISTGELANDET");
-		eins.setGeheimText("SVLEIFRXXJNKJFNTSVN");
+		eins.setPrivateKey("PRUEFUNGJDHKJSHDKJHSKJFDJKJFKLDJLFDH");
+		eins.setKlarText("DERADLERISTGELANDETAUFDEMBERG");
+		eins.setGeheimText("SVLEIFRXRVAQNDHQNNASEOIHVLNWQ");
 		
 		System.out.println(eins.codieren());
 		System.out.println();
